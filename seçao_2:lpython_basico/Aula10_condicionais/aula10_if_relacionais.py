f7fd0c366082ec4f80b0a2b4d7b3e334eb5e3f2d
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-06 10:05:23 -0300

    add and update

---
+++ seçao_2:lpython_basico/Aula10_condicionais/aula10_if_relacionais.py
@@ -0,0 +1,25 @@
+print(2 == 2)
+print(2 == 1)
+print(2 == '2')
+print('-' * 10)
+num = 2
+n = 2
+exp = num == n
+print(exp)
+print('-' * 10)
+nome = input('Qual seu nome? ')
+idade = int(input('Qual sua idade? '))
+idade_limite = 18
+if idade >= idade_limite:
+    print(f'{nome} pode pegar um empréstimos.')
+else:
+    print(f'{nome} não pode pegar empréstimos.')
+print('-' * 10)
+nome = input('Qual seu nome? ')
+idade = int(input('Qual sua idade? '))
+muito_jovem = 20
+muito_velho = 30
+if idade >= muito_jovem and idade <= muito_velho:
+    print(f'{nome} pode pegar um empréstimos.')
+else:
+    print(f'{nome} não pode pegar empréstimos.')
