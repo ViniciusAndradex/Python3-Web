342cfdf1dd311a6703fa435f291457cdcc54ad58
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-06 09:15:35 -0300

    add and update

---
+++ Aula8/aula8-desafio.py
@@ -0,0 +1,10 @@
+nome = 'Vinicius'
+idade = 21
+altura = 1.85
+peso = 89
+ano_atual = 2022
+imc = peso / altura ** 2
+
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}kg.')
+print(f'O IMC de {nome} é {imc:.2f}.')
+print(f'{nome} nasceu em {ano_atual - idade}.')
