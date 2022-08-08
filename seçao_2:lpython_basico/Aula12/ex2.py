2b8ac18248fee89d191207052e0fee3084bbd095
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-08 09:41:59 -0300

    Update ex2.py

--- seçao_2:lpython_basico/Aula12/ex2.py
+++ seçao_2:lpython_basico/Aula12/ex2.py
@@ -1,7 +1,7 @@
 num = input('Digite um horário (0-23): ')
 try:
     num = int(num)
-    if num <= 23:
+    if 0 <= num <= 23:
         if num <= 11:
             print(f'O horário é {num}:00, Bom dia!')
         elif num <= 17:
