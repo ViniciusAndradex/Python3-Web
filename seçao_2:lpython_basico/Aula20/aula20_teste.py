f3842026eb6dd2b6b9d39eb9c160ae9faf5f39be
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-09 16:52:06 -0300

    add

---
+++ seçao_2:lpython_basico/Aula20/aula20_teste.py
@@ -0,0 +1,10 @@
+def saudacao(msg='Ola,', nome='Jorge'):
+    nome = nome.replace('e', '3')
+    print(msg, nome)
+
+
+saudacao(msg='Plebeia')
+saudacao('Broxa', 'JOnaws')
+saudacao('Hello', 'Maria')
+saudacao()
+saudacao(msg='Preto', nome='Caralhoooo')
