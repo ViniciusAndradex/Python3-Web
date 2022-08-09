bebd6d4af71c6a07c58ea5a6feaf6705a168b3a3
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-09 15:18:01 -0300

    Create desafio_criador_cpf.py

---
+++ seçao_2:lpython_basico/Aula19/desafio_criador_cpf.py
@@ -0,0 +1,24 @@
+from random import randint
+
+# ADICONANDO MODULO PARA GERAR CPF's
+cpf = str(randint(100000000, 999999999))
+new_cpf = cpf
+total = index = 0
+c = 10
+
+# Criando a analise e adição dos números do digito 0.
+while True:
+    total += int(new_cpf[index]) * c
+    if c == 2:
+        digit = 11 - (total % 11)
+        if digit > 9:
+            digit = 0
+        new_cpf += str(digit)
+        if len(new_cpf) == 11:
+            break
+        total = index = 0
+        c = 11
+    c -= 1
+    index += 1
+
+print(f'CPG GERADO: {new_cpf}')
