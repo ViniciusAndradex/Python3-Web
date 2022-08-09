8aa1ee5af53a686c9b1fe7af4e426249799bc5ea
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-09 17:30:22 -0300

    Create desafio_func_3.py

---
+++ seçao_2:lpython_basico/Aula20/desafio_func_3.py
@@ -0,0 +1,9 @@
+def aumento(valor=0.0, percentual=0):
+    perc = valor * percentual / 100
+    return perc + valor
+
+
+dinheiro = aumento(100, 50)
+print(f'R${dinheiro:.2f}')
+print(f'R${aumento():.2f}')
+print(f'R${aumento(valor=200.9):.2f}')
