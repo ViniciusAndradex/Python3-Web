65cbbc6072968b24856b2928caa06cb0b78da1ab
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-09 17:51:02 -0300

    Update desafio_func_4.py

--- seçao_2:lpython_basico/Aula20/desafio_func_4.py
+++ seçao_2:lpython_basico/Aula20/desafio_func_4.py
@@ -1,12 +1,11 @@
 def fib(numero=0):
     if numero % 3 == 0 and numero % 5 == 0:
         return 'FizzBuzz'
-    elif numero % 3 == 0:
+    if numero % 3 == 0:
         return 'Fizz'
-    elif numero % 5 == 0:
+    if numero % 5 == 0:
         return 'Buzz'
-    else:
-        return numero
+    return numero
 
 
 retorno = fib(numero=int(input('Digite um número: ')))
