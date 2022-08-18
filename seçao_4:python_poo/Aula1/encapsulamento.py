16651dd7d2f036e733907b4c15febbbbe4d603de
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-18 15:29:17 -0300

    encapsulamento

---
+++ seçao_4:python_poo/Aula1/encapsulamento.py
@@ -0,0 +1,27 @@
+class BaseDeDados:
+    def __init__(self):
+        self.__dados = {}
+
+    @property
+    def dados(self):
+        return self.__dados
+
+    def inserir_clienete(self, id, nome):
+        if 'clientes' not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
+        else:
+            self.__dados['clientes'].update({id: nome})
+
+    def lista_clientes(self):
+        for id, nome in self.__dados['clientes'].items():
+            print(id, nome)
+
+    def apaga_cliente(self, id):
+        del self.__dados['clientes'][id]
+
+
+bd = BaseDeDados()
+bd.inserir_clienete(1, 'Rose')
+bd.inserir_clienete(2, 'Otávio')
+bd.inserir_clienete(3, 'Magal')
+print(bd.dados)
